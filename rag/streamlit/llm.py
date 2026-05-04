29ea388583f834e3c72f210d14d2cf3d683efdac
김희주 <ajtwlsgmlwn@naver.com>
2026-05-04 23:53:20 +0900

    fewshot 추가 적용

--- rag/streamlit/llm.py
+++ rag/streamlit/llm.py
@@ -1,5 +1,5 @@
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, FewShotChatMessagePromptTemplate
 from langchain_classic.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_openai import ChatOpenAI
@@ -8,6 +8,8 @@ from langchain_pinecone import PineconeVectorStore
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
+
+from config import answer_examples
 
 store = {}
 
@@ -25,27 +27,7 @@ def get_retriever():
     return retriever
 
 
-def get_llm(model="gpt-4o"):
-    llm = ChatOpenAI(model=model)
-    return llm
-
-
-def get_dictionary_chain():
-    dictionary = ["사람을 나타내는 표현 -> 거주자"]
-    llm = get_llm()
-    dictionary_prompt = ChatPromptTemplate.from_template(f"""
-        사용자의 질문을 보고, 우리의 사전을 참고해서 사용자의 질문을 변경해주세요.
-        만약 변경할 필요가 없다고 판단되면, 사용자의 질문을 변경하지 않아도 됩니다.
-        변경이 필요 없는 경우에는 질문만 반환해주세요.
-        사전: {dictionary}
-                                            
-        질문: {{question}}
-    """)
-    dictionary_chain = dictionary_prompt | llm | StrOutputParser()
-    return dictionary_chain
-
-
-def get_rag_chain():
+def get_history_retriever():
     llm = get_llm()
     retriever = get_retriever()
     
@@ -72,11 +54,48 @@ def get_rag_chain():
         llm, retriever, contextualize_q_prompt,
     )
 
-    system_prompt = """
-    당신은 질문-답변 작업을 수행하는 어시스턴트입니다.
-    아래에 제공된 검색 문맥을 사용하여 질문에 답변하세요.
-    답을 모르는 경우에는 모른다고 말하세요.
+    return history_aware_retriever
 
+
+def get_llm(model="gpt-4o"):
+    llm = ChatOpenAI(model=model)
+    return llm
+
+
+def get_dictionary_chain():
+    dictionary = ["사람을 나타내는 표현 -> 거주자"]
+    llm = get_llm()
+    dictionary_prompt = ChatPromptTemplate.from_template(f"""
+        사용자의 질문을 보고, 우리의 사전을 참고해서 사용자의 질문을 변경해주세요.
+        만약 변경할 필요가 없다고 판단되면, 사용자의 질문을 변경하지 않아도 됩니다.
+        변경이 필요 없는 경우에는 질문만 반환해주세요.
+        사전: {dictionary}
+                                            
+        질문: {{question}}
+    """)
+    dictionary_chain = dictionary_prompt | llm | StrOutputParser()
+    return dictionary_chain
+
+
+def get_rag_chain():
+    llm = get_llm()
+    example_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("human", "{input}"),
+            ("ai", "{answer}"),
+        ]
+    )
+    few_shot_prompt = FewShotChatMessagePromptTemplate(
+        example_prompt=example_prompt,
+        examples=answer_examples,
+    )
+    system_prompt = """
+    당신은 소득세법 전문가입니다. 사용자의 소득세법에 관한 질문에 답변해주세요.
+    아래에 제공된 문서를 활용해서 답변해주시고,
+    답변을 알 수 없다면 모른다고 답변해주세요.
+    답변을 제공할 때는 소득세법 (XX조)에 따르면 이라고 시작하면서 답변해주시고
+    2-3 문장정도의 짧은 내용의 답변을 원합니다.
+    
     context:
     {context}
     """
@@ -84,11 +103,13 @@ def get_rag_chain():
     qa_prompt = ChatPromptTemplate.from_messages(
         [
             ("system", system_prompt),
+            few_shot_prompt,
             MessagesPlaceholder("chat_history"),
             ("human", "{input}"),
         ]
     )
 
+    history_aware_retriever = get_history_retriever()
     question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
     
     rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
